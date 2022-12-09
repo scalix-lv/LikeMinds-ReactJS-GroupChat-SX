ed94edc7648adcc3f409da85d8687a83569250e8
Sanjay Kumar <sanjay.kumar@likeminds.community>
2022-12-09 12:08:50 +0530

    Style releated changes

--- src/components/channelGroups/MessageBlock.js
+++ src/components/channelGroups/MessageBlock.js
@@ -1,24 +1,26 @@
-import { Box } from '@mui/material'
-import React, { useState } from 'react'
-import { userObj } from '../..'
-import MessageBox from './MessageBox'
+import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { userObj } from "../..";
+import MessageBox from "./MessageBox";
 
-function MessageBlock({
-    conversationObject, userId
-}) {
-  const [convoDetails, setConvoDetails] = useState(conversationObject)
-    const currentUser = userObj.id
+function MessageBlock({ conversationObject, userId }) {
+  const [convoDetails, setConvoDetails] = useState(conversationObject);
+  const currentUser = userObj.id;
   return (
     <Box
-    className='flex py-2 px-0'
-    sx={{
-        
-        flexDirection: userId === currentUser ? 'row-reverse' : 'row',
-        
-    }}>
-        <MessageBox userId={userId} username={convoDetails.member.name} messageString={convoDetails.answer} time={convoDetails.created_at}/>
+      className="flex py-2 px-0 h-full overflow-auto"
+      sx={{
+        flexDirection: userId === currentUser ? "row-reverse" : "row",
+      }}
+    >
+      <MessageBox
+        userId={userId}
+        username={convoDetails.member.name}
+        messageString={convoDetails.answer}
+        time={convoDetails.created_at}
+      />
     </Box>
-  )
+  );
 }
 
-export default MessageBlock
+export default MessageBlock;
