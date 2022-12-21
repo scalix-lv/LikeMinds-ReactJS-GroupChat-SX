42652ea7e9ca4587c2e470bf86b3eda37f7cf022
Sanjay Kumar <sanjay.kumar@likeminds.community>
2022-12-21 12:34:31 +0530

    group info and profile page ui changes

--- src/components/groupChatArea/GroupInfo.js
+++ src/components/groupChatArea/GroupInfo.js
@@ -1,89 +1,79 @@
-import { Box, IconButton, Typography } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import React, { useContext } from "react";
-import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import styled from "@emotion/styled";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import { Link } from "react-router-dom";
 import { GroupContext } from "../Groups/Groups";
-// import IconButton from '@mui/material';
+import Tittle from "./tittle/Tittle";
+import backIcon from "../../assets/svg/arrow-left.svg";
+import rightArrow from "../../assets/svg/right-arrow.svg";
 
 const StyledBox = styled(Box)({
   backgroundColor: "#f6f6ff",
-  minHeight: "100vh",
-  display: "flex",
-  flexDirection: "column",
 });
 
 function GroupInfo() {
   const gc = useContext(GroupContext);
-  console.log(gc);
   const participants = gc.activeGroup.membersDetail;
   return (
-    <StyledBox
-      style={{
-        marginTop: "none",
-        padding: "0px 96px 0px 12px",
-      }}
-    >
-      <Box className="flex items-center">
-        <Link to={"/groups/main"}>
-          <IconButton>
-            <KeyboardBackspaceIcon
-              sx={{
-                color: "#3884F7",
-              }}
-            />
-          </IconButton>
-        </Link>
-        <Typography fontSize={"20px"} fontWeight={700} color={"#3884F7"}>
-          Group Info
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          marginLeft: "12px",
-        }}
-      >
-        This group is a community of people working in start- ups coming
-        together to help each other.
-        <Typography
-          fontSize={"16px"}
-          fontWeight={700}
-          color={"#323232"}
-          marginTop={2}
-        >
-          Participants
-        </Typography>
-        <Box className="py-6">
-          {participants.map((profile, profileIndex) => {
-            return (
-              <ParticipantTile
-                key={profile + profileIndex}
-                profile={profile}
-                index={profileIndex}
-              />
-            );
-          })}
-        </Box>
-      </Box>
+    <StyledBox>
+      {/* Title Header */}
+      {gc.activeGroup.chatroom?.id ? (
+        <Tittle
+          headerProps={{
+            title: gc.activeGroup.chatroom.header,
+            memberCount: gc.activeGroup.participant_count,
+          }}
+        />
+      ) : null}
+      {/* Title Header */}
+
+      <div className="mr-[120px] ml-[20px] mt-[10px]">
+        <div className="flex">
+          <Link to={"/groups/main"}>
+            <IconButton>
+              <img src={backIcon} alt="back icon" />
+            </IconButton>
+          </Link>
+          <div className="text-[20px] mt-[8px] font-[700] leading-[24px] text-[#3884F7]">
+            Group Info
+          </div>
+        </div>
+
+        {/* Member list */}
+        <div className="ml-1 pl-[5px]">
+          <div className="text-4 font-[700] text-[#323232]">Participants</div>
+          <div className="py-[18px]">
+            {participants.map((profile, profileIndex) => {
+              return (
+                <ParticipantTile
+                  key={profile + profileIndex}
+                  profile={profile}
+                  index={profileIndex}
+                />
+              );
+            })}
+          </div>
+        </div>
+        {/* Member list */}
+      </div>
     </StyledBox>
   );
 }
 
 function ParticipantTile({ index, profile }) {
   return (
-    <Box className="py-3 px-2  border-gray border flex justify-center items-center bg-white">
-      <AccountCircleIcon fontSize="large" className="mr-3" />
+    <div className="p-2.5 border-[#eeeeee] border-b-[1px] flex justify-between bg-white items-center">
+      <div className="flex items-center">
+        <div className="w-[36px] h-[36px] border-[1px] border-[#eeeeee] mr-2.5 rounded-[5px] flex justify-center items-center">
+          {profile?.name[0]}
+        </div>
+        <div className="font-bold text-sm">{profile.name}</div>
+      </div>
 
-      <span className="font-bold text-sm">
-        {profile.name} {index}
-      </span>
-      <div className="grow" />
-      <IconButton>
-        <KeyboardArrowRightIcon />
+      <IconButton className="w-[32px] h-[32px]">
+        <img src={rightArrow} alt="arrow icon" />
       </IconButton>
-    </Box>
+    </div>
   );
 }
 export default GroupInfo;
--- src/components/groupChatArea/PersonInfo.js
+++ src/components/groupChatArea/PersonInfo.js
@@ -1,22 +1,23 @@
 // import { AccountCircleIcon } from '@mui/icons-material'
 import { Box, Button } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
-// import styled from '@emotion/styled';
-// import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { StyledBox } from "./GroupChatArea";
 import { IconButton } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Link, useLocation } from "react-router-dom";
 import { myClient } from "../..";
+import backIcon from "../../assets/svg/arrow-left.svg";
 
 import userIcon from "./../../assets/user.png";
+import { GroupContext } from "../Groups/Groups";
+import Tittle from "./tittle/Tittle";
+
 function PersonInfo() {
+  const gc = useContext(GroupContext);
   const mediaArray = [LinkedInIcon, InstagramIcon, TwitterIcon];
   const location = useLocation();
   const [profileDate, setProfileData] = useState({});
@@ -36,69 +37,81 @@ function PersonInfo() {
     fn();
   }, []);
   return (
-    <StyledBox
-      style={{
-        marginTop: "none",
-        padding: "0px 96px 0px 12px",
-      }}
-    >
-      <Box className="flex items-center">
-        <Link to={"/groups/main"}>
-          <IconButton>
-            <KeyboardBackspaceIcon />
-          </IconButton>
-        </Link>
-        <Typography fontSize={"20px"} fontWeight={700} color={"#3884F7"}>
-          Group Info / {profileDate.name}
-        </Typography>
-      </Box>
-      <Box className="ml-3 mt-4">
-        <Box>
-          {/* <AccountCircleIcon fontSize="large" /> */}
+    <div>
+      {/* Title Header */}
+      {gc.activeGroup.chatroom?.id ? (
+        <Tittle
+          headerProps={{
+            title: gc.activeGroup.chatroom.header,
+            memberCount: gc.activeGroup.participant_count,
+          }}
+        />
+      ) : null}
+      {/* Title Header */}
 
-          <img
-            src={profileDate.image_url || userIcon}
-            className="w-[60px] h-[60px] rounded-[5px]"
-            alt=""
-          />
+      <div className="mr-[120px] ml-[20px] mt-[10px]">
+        <div className="flex">
+          <Link to={"/groups/main"}>
+            <IconButton>
+              <img src={backIcon} alt="back icon" />
+            </IconButton>
+          </Link>
+          <div className="text-[20px] mt-[8px] font-[400] leading-[24px]">
+            Group Info /{" "}
+            <span className="font-[700] text-[#3884F7]">
+              {profileDate.name}
+            </span>
+          </div>
+        </div>
+
+        <Box className="ml-3 mt-4">
+          <Box>
+            {/* <AccountCircleIcon fontSize="large" /> */}
+
+            <img
+              src={profileDate.image_url || userIcon}
+              className="w-[60px] h-[60px] rounded-[5px]"
+              alt=""
+            />
+          </Box>
+          <p className="text-base font-bold my-2 mx-0 text-[#323232]">
+            {profileDate.name}
+          </p>
+          <Box className="my-2 mx-0">
+            {mediaArray.map((MediaIcon, mediaIconIndex) => {
+              return <MediaIcon className="m-1 text-4xl" fontSize="medium" />;
+            })}
+          </Box>
+          <Typography
+            fontSize={"14px"}
+            fontWeight={400}
+            color={"#323232"}
+            marginTop={2}
+          >
+            An organized and enthusiastic designer, whose life has been nothing
+            but a series of unplanned and unexpected events. I enjoy working on
+            topics that are out of the box and that would let me come up with
+            innovative ideas.
+          </Typography>
+
+          <Typography
+            fontSize={"16px"}
+            fontWeight={700}
+            color={"#323232"}
+            marginTop={2}
+          >
+            {profileDate.custom_title}
+          </Typography>
+
+          <Box className="py-4 px-0">
+            <p className="font-bold text-2xl">Find them in</p>
+
+            <InfoTile title={"Hiring Techniques"} buttontitle={"Forum"} />
+            <InfoTile title={"Beyond Design"} buttontitle={"Group"} />
+          </Box>
         </Box>
-        <p className="text-base font-bold my-2 mx-0 text-[#323232]">
-          {profileDate.name}
-        </p>
-        <Box className="my-2 mx-0">
-          {mediaArray.map((MediaIcon, mediaIconIndex) => {
-            return <MediaIcon className="m-1 text-4xl" fontSize="medium" />;
-          })}
-        </Box>
-        <Typography
-          fontSize={"14px"}
-          fontWeight={400}
-          color={"#323232"}
-          marginTop={2}
-        >
-          An organized and enthusiastic designer, whose life has been nothing
-          but a series of unplanned and unexpected events. I enjoy working on
-          topics that are out of the box and that would let me come up with
-          innovative ideas.
-        </Typography>
-
-        <Typography
-          fontSize={"16px"}
-          fontWeight={700}
-          color={"#323232"}
-          marginTop={2}
-        >
-          {profileDate.custom_title}
-        </Typography>
-
-        <Box className="py-4 px-0">
-          <p className="font-bold text-2xl">Find them in</p>
-
-          <InfoTile title={"Hiring Techniques"} buttontitle={"Forum"} />
-          <InfoTile title={"Beyond Design"} buttontitle={"Group"} />
-        </Box>
-      </Box>
-    </StyledBox>
+      </div>
+    </div>
   );
 }
 
