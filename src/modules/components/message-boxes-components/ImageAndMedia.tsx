7bbfd5a732c74aa0b0fc6a5312c0ad1173051042
Omen Uchiha <omen@LikeMindss-MacBook-Pro.local>
2023-04-29 19:01:31 +0530

    segregated the attachments field

---
+++ src/modules/components/message-boxes-components/ImageAndMedia.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+type ImageAndMediaType = {
+  mediaArray: any;
+};
+const ImageAndMedia = ({ mediaArray }: ImageAndMediaType) => {
+  return (
+    <>
+      <div className="flex">
+        {mediaArray?.length === 1 ? (
+          <div className="w-full">
+            {mediaArray[0].type == "image" ? (
+              <img
+                src={mediaArray[0].url!}
+                className="max-w-full max-h-full block h-auto w-auto"
+              />
+            ) : (
+              <video
+                controls={true}
+                preload="none"
+                className="max-w-full max-h-full block h-auto w-auto"
+                key={mediaArray[0]?.url}
+                //   onClick={}
+              >
+                <source src={mediaArray[0]?.url} type="video/mp4" />
+                <source src={mediaArray[0]?.url} type="video/ogg" />
+                Your browser does not support the video tag.
+              </video>
+            )}
+          </div>
+        ) : (
+          <>
+            <div className="max-w-[50%] h-full">
+              {mediaArray[0].type == "image" ? (
+                <img
+                  src={mediaArray[0].url!}
+                  className="max-w-full max-h-full block h-auto w-auto"
+                />
+              ) : (
+                <video
+                  controls={true}
+                  preload="none"
+                  className="max-w-full max-h-full block h-auto w-auto"
+                  key={mediaArray[0]?.url}
+                  //   onClick={}
+                >
+                  <source src={mediaArray[0]?.url} type="video/mp4" />
+                  <source src={mediaArray[0]?.url} type="video/ogg" />
+                  Your browser does not support the video tag.
+                </video>
+              )}
+            </div>
+            <div
+              className="w-full"
+              style={{
+                opacity: "50%",
+              }}
+            >
+              <div
+                className="flex justify-center items-center grow bg-contain h-full"
+                style={{
+                  background: `url(${mediaArray[1]?.url})`,
+                  backgroundSize: "cover",
+                  backgroundRepeat: "no-repeat",
+                  position: "relative",
+                }}
+              >
+                <span className="text-xl text-black  text-center">
+                  + {(mediaArray.length - 1).toString()}
+                </span>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ImageAndMedia;
